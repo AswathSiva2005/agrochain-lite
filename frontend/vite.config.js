3758407e3cba69f8616fa90a59a83df3d13aee42
AswathSiva2005 <aswathsiva0420@gmail.com>
2025-08-24 22:12:48 +0530

    error when i refresh so corrected again 2time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true,
+    port: 5173,
+    host: true
   },
   preview: {
     historyApiFallback: true,
+    port: 4173
+  },
+  build: {
+    outDir: 'dist'
   }
 })
